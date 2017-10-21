75fa87eb4fac891112eed5969efd743e7c3118c8
Wes D <wesdoyle08@gmail.com>
2017-10-21 11:51:06 -0500

    Add customer methods

--- Advantage.API.Demo/Controllers/CustomerController.cs
+++ Advantage.API.Demo/Controllers/CustomerController.cs
@@ -1,5 +1,7 @@
 ﻿using Advantage.API.Demo.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Advantage.API.Demo.Controllers
 {
@@ -14,34 +16,70 @@ namespace Advantage.API.Demo.Controllers
 
         // GET api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Customer> Get()
         {
-            return new string[] { "value1", "value2" };
+            return _ctx.Customers;
         }
 
         // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("{id}", Name = "GetCustomer")]
+        public Customer Get(int id)
         {
-            return "value";
+            return _ctx.Customers.Find(id);
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public IActionResult Post([FromBody] Customer customer)
         {
+            if (customer == null)
+            {
+                return BadRequest();
+            }
+
+            _ctx.Customers.Add(customer);
+            _ctx.SaveChanges();
+
+            return CreatedAtRoute("GetCustomer", new { id = customer.Id }, customer);
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public IActionResult Put(int id, [FromBody] Customer customer)
         {
+            if (customer == null || customer.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var updatedCustomer = _ctx.Customers.FirstOrDefault(c => c.Id == id);
+
+            if (updatedCustomer == null)
+            {
+                return NotFound();
+            }
+
+            updatedCustomer.Email = customer.Email;
+            updatedCustomer.Name = customer.Name;
+            updatedCustomer.State = customer.State;
+
+            _ctx.SaveChanges();
+            return new NoContentResult();
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            var customer = _ctx.Customers.FirstOrDefault(t => t.Id == id);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            _ctx.Customers.Remove(customer);
+            _ctx.SaveChanges();
+            return new NoContentResult();
         }
     }
 }
---
+++ Advantage.API.Demo/PaginatedResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Advantage.API.Demo
+{
+    public class PaginatedResponse<T>
+    {
+        public PaginatedResponse(IEnumerable<T> data, int i, int len)
+        {
+            Data = data.Skip((i - 1) * len).Take(len).ToList();
+            Total = data.Count();
+        }
+
+        public int Total { get; set; }
+        public IEnumerable<T> Data { get; set; }
+    }
+}
